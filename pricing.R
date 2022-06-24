0f298b432ab527f8d6696ceee292351e591871ea
rafael747cardoso <rafael747cardoso@gmail.com>
2022-06-24 18:29:21 -0300

    regularization and dim reduc

--- pricing.R
+++ pricing.R
@@ -39,6 +39,7 @@ id_var = "Id"
 df_test[response_var] = -1 # just to know it is the test set
 df_all = rbind(df_train,
                df_test)
+df_all_orig = df_all
 
 # Fix the variable names starting with a number:
 vars_names = names(df_all)
@@ -78,9 +79,15 @@ Amelia::missmap(df_all)
 
 # Types of variables:
 cat_vars = df_all %>%
+               dplyr::select(-all_of(response_var),
+                             -all_of(id_var)) %>%
                dplyr::select_if(~!is.numeric(.)) %>%
                names()
-num_vars = names(df_all)[which(!(names(df_all) %in% c(response_var, cat_vars, id_var)))]
+num_vars = df_all %>%
+               dplyr::select(-all_of(response_var),
+                             -all_of(id_var)) %>%
+               dplyr::select_if(~is.numeric(.)) %>%
+               names()
 
 # Replace the remaining NA's with the mean (for numeric) or the mode (for categoric) in each column:
 for(var in num_vars){
@@ -101,13 +108,22 @@ for(var in cat_vars){
 }
 
 # Dummies:
-df_all = fastDummies::dummy_cols(.data = df_all,
-                                 select_columns = cat_vars,
-                                 remove_selected_columns = TRUE,
-                                 remove_first_dummy = TRUE)
-dummy_vars = names(df_all)[apply(X = df_all,
-                                 MARGIN = 2,
-                                 FUN = is_dummy)]
+df_all_cats_dumm = fastDummies::dummy_cols(.data = df_all %>%
+                                                       dplyr::select(all_of(cat_vars)),
+                                           select_columns = cat_vars,
+                                           remove_selected_columns = TRUE,
+                                           remove_first_dummy = TRUE)
+dummy_vars = names(df_all_cats_dumm)
+
+# Now it has a total of length(num_vars) + 1 response + 1 id + length(df_all_cats_dumm) variables:
+df_all = cbind(df_all %>%
+                   dplyr::select(-all_of(cat_vars)),
+               df_all_cats_dumm)
+p_predictors = df_all %>%
+                   dplyr::select(-all_of(response_var),
+                                 -all_of(id_var)) %>%
+                   names()
+predictors_not_dummies = num_vars
 
 # Back to the train/test split:
 df_train = df_all %>%
@@ -117,12 +133,6 @@ df_test = df_all %>%
               dplyr::select(-all_of(response_var))
 
 ### Standardize the predictors from training and test
-
-# For this, the variables must be in camelCase:
-predictors = names(df_train)[!(names(df_train) %in% c(response_var, id_var))]
-predictors_not_dummies = predictors[!(predictors %in% dummy_vars)]
-predictors_not_dummies = predictors_not_dummies[!grepl(x = predictors_not_dummies,
-                                                       pattern = "_")]
 
 # Training:
 df_train_prdnotdum = df_train[predictors_not_dummies]
@@ -208,7 +218,6 @@ cv_mse_mlr = function(predictors, df_model){
 
 ### K-Fold Cross-validated MSE
 
-p_predictors = names(df_train_forward)[names(df_train_forward) != response_var]
 cv_mse = c()
 cv_mse_se = c()
 num_predictors = c()
@@ -272,6 +281,7 @@ df_eval = data.frame(
     "predictors" = predictors_names
 )
 df_eval$cv_mse_se[is.na(df_eval$cv_mse_se)] = 0
+saveRDS(df_eval, "./data/df_eval_forward.rds")
 
 # Best model with the 1-standard-error rule:
 min_cv_mse = min(df_eval$cv_mse)
@@ -285,7 +295,6 @@ best_predictors = (df_eval %>%
                        dplyr::filter(num_predictors == best_p))$predictors
 best_predictors = strsplit(x = best_predictors,
                            split = ",")[[1]]
-saveRDS(df_eval, "./data/df_eval_forward.rds")
 
 # Plot:
 make_subset_selection_plot(df_eval = df_eval,
@@ -303,36 +312,433 @@ df_model = df_train_forward %>%
                              all_of(response_var))
 fit_forward = glm(formula = SalePrice ~ .,
               data = df_model)
-multi_reg_plots(model_fit = fit_forward)
 saveRDS(fit_forward, "./data/fit_forward.rds")
+multi_reg_plots(model_fit = fit_forward)
 
 ###### Ridge Regression
 
+# Matrix data:
+X = as.matrix(df_train_ridge[, p_predictors])
+Y = df_train_ridge[, response_var]
 
+# Fit:
+fit_ridge = glmnet::glmnet(x = X,
+                           y = Y,
+                           alpha = 0,
+                           standardize = FALSE,
+                           family = "gaussian")
 
+# K-Fold Cross-validation:
+cv_ridge = glmnet::cv.glmnet(x = X,
+                             y = Y,
+                             alpha = 0,
+                             type.measure = "mse",
+                             nfolds = 10,
+                             standardize = FALSE,
+                             family = "gaussian")
 
+# Plot:
+make_shrinkage_plot(cv = cv_ridge,
+                    model_type = "Ridge Regression",
+                    fig_path = "./figs/Ridge_Regression.png")
+
+# Estimated Test Prediction Error:
+test_mse_ridge = cv_ridge$cvm[which(cv_ridge$lambda == cv_ridge$lambda.1se)]
+test_mse_ridge
 
 ###### The Lasso
 
+# Matrix data:
+X = as.matrix(df_train_lasso[, p_predictors])
+Y = df_train_lasso[, response_var]
 
+# Fit:
+fit_lasso = glmnet::glmnet(x = X,
+                           y = Y,
+                           alpha = 1,
+                           standardize = FALSE,
+                           family = "gaussian")
 
+# K-Fold Cross-validation:
+cv_lasso = glmnet::cv.glmnet(x = X,
+                             y = Y,
+                             alpha = 1,
+                             type.measure = "mse",
+                             nfolds = 10,
+                             standardize = FALSE,
+                             family = "gaussian")
+
+# Plot:
+make_shrinkage_plot(cv = cv_lasso,
+                    model_type = "The Lasso",
+                    fig_path = "./figs/The_Lasso.png")
+
+# Estimated Test Prediction Error:
+test_mse_lasso = cv_lasso$cvm[which(cv_lasso$lambda == cv_lasso$lambda.1se)]
+test_mse_lasso
 
 ###### Principal Components Regression
 
+# Fit:
+fit_pcr = pls::pcr(formula = SalePrice ~ .,
+                   data = df_train_pcr,
+                   scale = FALSE,
+                   validation = "CV",
+                   segments = 10)
 
+# K-Fold Cross-validation:
+cv_pcr = pls::RMSEP(fit_pcr)
+p = length(p_predictors)
+m = 1:p
+cv_pcr_MSE = (cv_pcr$val[seq(from = 3,
+                             to = 2*p + 1,
+                             by = 2)])**2
+best_m = pls::selectNcomp(fit_pcr,
+                          method = "onesigma",
+                          plot = FALSE)
+df_plot = data.frame(
+    "m" = m,
+    "cv_mse" = cv_pcr_MSE
+)
+df_best = data.frame(
+    "best_m" = best_m,
+    "cv_mse_best_m" = (df_plot %>%
+                          dplyr::filter(m == best_m))$cv_mse[1]
+)
 
+# Plot:
+make_dim_reduc_plot(df_plot = df_plot,
+                    df_best = df_best)
+
+# Estimated Test Prediction Error:
+test_mse_pcr = df_best$cv_mse_best_m[1]
+test_mse_pcr
 
 ###### Partial Least Squares
 
+# Fit:
+fit_pls = pls::plsr(formula = SalePrice ~ .,
+                    data = df_train_pls,
+                    scale = FALSE,
+                    validation = "CV",
+                    segments = 10)
 
+# K-Fold Cross-validation:
+cv_pls = pls::RMSEP(fit_pls)
+p = length(p_predictors)
+m = 1:p
+cv_pls_MSE = (cv_pls$val[seq(from = 3,
+                             to = 2*p + 1,
+                             by = 2)])**2
+best_m = pls::selectNcomp(fit_pls,
+                          method = "onesigma",
+                          plot = FALSE)
+df_plot = data.frame(
+    "m" = m,
+    "cv_mse" = cv_pls_MSE
+)
+df_best = data.frame(
+    "best_m" = best_m,
+    "cv_mse_best_m" = (df_plot %>%
+                          dplyr::filter(m == best_m))$cv_pls[1]
+)
 
+# Plot:
+make_dim_reduc_plot(df_plot = df_plot,
+                    df_best = df_best)
+
+# Estimated Test Prediction Error:
+test_mse_pls = df_best$cv_mse_best_m[1]
+test_mse_pls
 
 ###### Comparison
 
-# Plot:
+### Plot the estimated test Prediction Error by type of tuning parameter
+
+# by lambda (Ridge and Lasso):
+best_lambda_ridge = cv_ridge$lambda.1se
+df_mse_ridge = data.frame(
+    "cv_mse" = cv_ridge$cvm,
+    "cv_mse_se" = cv_ridge$cvsd,
+    "lambdas" = cv_ridge$lambda,
+    "d" = fit_ridge$df
+)
+best_lambda_lasso = cv_lasso$lambda.1se
+df_mse_lasso = data.frame(
+    "cv_mse" = cv_lasso$cvm,
+    "cv_mse_se" = cv_lasso$cvsd,
+    "lambdas" = cv_lasso$lambda,
+    "d" = fit_lasso$df
+)
+color_ridge = "#f68105"
+color_lasso = "#0591f6"
+p1 = ggplot() +
+    # Ridge:
+    geom_point(
+        data = df_mse_ridge,
+        aes(
+            x = log(lambdas),
+            y = cv_mse,
+            color = "Ridge"
+        ),
+        size = 2
+    ) +
+    geom_errorbar(
+        data = df_mse_ridge,
+        aes(
+            x = log(lambdas),
+            y = cv_mse,
+            ymin = cv_mse - cv_mse_se,
+            ymax = cv_mse + cv_mse_se
+        ),
+        color = color_ridge,
+        width = 0.1
+    ) +
+    geom_vline(
+        aes(
+            xintercept = log(cv_ridge$lambda.min),
+            color = "Ridge Minimum Lambda"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_vline(
+        aes(
+            xintercept = log(best_lambda_ridge),
+            color = "Ridge Largest Lambda within\n1SE of the minimum"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_hline(
+        yintercept = cv_ridge$cvm[which(cv_ridge$lambda == best_lambda_ridge)] - 
+                     cv_ridge$cvsd[which(cv_ridge$lambda == best_lambda_ridge)],
+        color = "grey",
+        linetype = "dashed"
+    ) +
+    # Lasso:
+    geom_point(
+        data = df_mse_lasso,
+        aes(
+            x = log(lambdas),
+            y = cv_mse,
+            color = "Lasso"
+        ),
+        size = 2
+    ) +
+    geom_errorbar(
+        data = df_mse_lasso,
+        aes(
+            x = log(lambdas),
+            y = cv_mse,
+            ymin = cv_mse - cv_mse_se,
+            ymax = cv_mse + cv_mse_se
+        ),
+        color = color_lasso,
+        width = 0.1
+    ) +
+    geom_vline(
+        aes(
+            xintercept = log(cv_lasso$lambda.min),
+            color = "Lasso Minimum Lambda"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_vline(
+        aes(
+            xintercept = log(best_lambda_lasso),
+            color = "Lasso Largest Lambda within\n1SE of the minimum"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_hline(
+        yintercept = cv_lasso$cvm[which(cv_lasso$lambda == best_lambda_lasso)] - 
+                     cv_lasso$cvsd[which(cv_lasso$lambda == best_lambda_lasso)],
+        color = "grey",
+        linetype = "dashed"
+    ) +
+    scale_colour_manual(
+        values = c("Ridge" = color_ridge,
+                   "Ridge Minimum Lambda" = "#d1976b",
+                   "Ridge Largest Lambda within\n1SE of the minimum" = "#b75309",
+                   "Lasso" = color_lasso,
+                   "Lasso Minimum Lambda" = "#6a91cc",
+                   "Lasso Largest Lambda within\n1SE of the minimum" = "#0e469a"),
+        guide = guide_legend(ncol = 2)
+    ) +
+    theme(
+        axis.text.x = element_text(
+            size = 14,
+            angle = 0,
+            hjust = 0.5,
+            vjust = 1
+        ),
+        axis.text.y = element_text(
+            size = 14
+        ),
+        axis.title.x = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        axis.title.y = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        panel.background = element_rect(
+            fill = "white"
+        ),
+        panel.grid.major = element_line(
+            size = 0.2,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        panel.grid.minor = element_line(
+            size = 0.1,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        legend.title = element_blank(),
+        legend.text = element_text(
+            size = 12
+        ),
+        legend.position = "top",
+        legend.background = element_rect(
+            fill = "transparent"
+        )
+    ) +
+    xlab("Log(lambda)") +
+    ylab(cv_ridge$name)
+
+# by number of components (PCR and PLS):
+p = length(p_predictors)
+best_m_pcr = pls::selectNcomp(fit_pcr,
+                              method = "onesigma",
+                              plot = FALSE)
+df_mse_pcr = data.frame(
+    "m" = 1:p,
+    "cv_mse" = cv_pcr_MSE
+)
+best_m_pls = pls::selectNcomp(fit_pls,
+                              method = "onesigma",
+                              plot = FALSE)
+df_mse_pls = data.frame(
+    "m" = 1:p,
+    "cv_mse" = cv_pls_MSE
+)
+color_pcr = "#43c41a"
+color_pls = "#8621c4"
+p2 = ggplot() +
+    # PCR:
+    geom_point(
+        data = df_mse_pcr,
+        aes(
+            x = m,
+            y = cv_mse,
+            color = "PCR"
+        ),
+        size = 2
+    ) +
+    geom_vline(
+        aes(
+            xintercept = best_m_pcr,
+            color = "PCR Largest m within\n1SE of the minimum"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_hline(
+        yintercept = df_mse_pcr$cv_mse[which(df_mse_pcr$m == best_m_pcr)],
+        color = "grey",
+        linetype = "dashed"
+    ) +
+    # PLS:
+    geom_point(
+        data = df_mse_pls,
+        aes(
+            x = m,
+            y = cv_mse,
+            color = "PLS"
+        ),
+        size = 2
+    ) +
+    geom_vline(
+        aes(
+            xintercept = best_m_pls,
+            color = "PLS Largest m within\n1SE of the minimum"
+        ),
+        size = 1,
+        linetype = "dashed",
+        show.legend = TRUE
+    ) +
+    geom_hline(
+        yintercept = df_mse_pls$cv_mse[which(df_mse_pls$m == best_m_pls)],
+        color = "grey",
+        linetype = "dashed"
+    ) +
+    scale_colour_manual(
+        values = c("PCR" = color_pcr,
+                   "PCR Largest m within\n1SE of the minimum" = "#2f8d11",
+                   "PLS" = color_pls,
+                   "PLS Largest m within\n1SE of the minimum" = "#722c80"),
+        guide = guide_legend(ncol = 2)
+    ) +
+    theme(
+        axis.text.x = element_text(
+            size = 14,
+            angle = 0,
+            hjust = 0.5,
+            vjust = 1
+        ),
+        axis.text.y = element_text(
+            size = 14
+        ),
+        axis.title.x = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        axis.title.y = element_text(
+            size = 15,
+            face = "bold"
+        ),
+        panel.background = element_rect(
+            fill = "white"
+        ),
+        panel.grid.major = element_line(
+            size = 0.2,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        panel.grid.minor = element_line(
+            size = 0.1,
+            linetype = "solid",
+            colour = "#eaeaea"
+        ),
+        legend.title = element_blank(),
+        legend.text = element_text(
+            size = 12
+        ),
+        legend.position = "top",
+        legend.background = element_rect(
+            fill = "transparent"
+        )
+    ) +
+    xlab("Number of components") +
+    ylab("CV MSE")
+
+
+
 
 # Best model:
 # best_fit = fit_ ...
+
+
 
 
 
